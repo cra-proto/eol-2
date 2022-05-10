a4c0c8d172bba65c0f80b5a8067cad2f98a40bc6
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-05-10 07:44:57 -0600

    Update feedback-banner.js
    updated list of webpages to redirect to 404.

--- js/feedback-banner.js
+++ js/feedback-banner.js
@@ -12,6 +12,7 @@ $( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
 	$("a[href='https://www.canada.ca/en/services/finance/manage.html']").attr("href", "https://cra-proto.github.io/eol-2/services/finance/manage.html").addClass("prototype");
 	$(".gcweb-menu a:not(.prototype)").attr("href", "https://cra-proto.github.io/eol-2/validation/404.html");	
 	$("a[href*='canada.ca']").attr("href", "https://cra-proto.github.io/eol-2/validation/404.html");
+	$("a[href*='.gc.ca/']").attr("href", "https://cra-proto.github.io/eol/validation/404.html");
 });
 
 //Seperate script to null all canada.ca links to a 404 page
